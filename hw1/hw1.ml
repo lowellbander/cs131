06ac09b6f5be1db38d48a620bb44d0643a77a550
Lowell Bander <lowellbander@gmail.com>
2015-01-17 00:38:45 -0500

    actually removed tests from main

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -5,10 +5,6 @@ let rec contains p q =
    | hd::tl -> if hd=q then true else (contains tl q)
 ;;
 
-let contains_test_0 = contains [1;2;3] 1;;
-let contains_test_1 = not (contains [1;2;3] 4);;
-let contains_test_2 = not (contains [] 4);;
-
 (* returns true iff a subset of b *)
 let rec subset a b = 
     match a with
@@ -16,23 +12,9 @@ let rec subset a b =
     | hd::tl -> if (contains b hd ) then (subset tl b) else false
 ;;
 
-let subset_test_0 = subset [] [];;
-let subset_test_1 = subset [] [1];;
-let subset_test_2 = subset [] [1;2];;
-let subset_test_3 = not (subset [1] []);;
-let subset_test_4 = subset [1;2;3] [1;2;3];;
-let subset_test_5 = not(subset [1;2;3] [1;2;4]);;
-
 let equal_sets a b =
     if (subset a b) then (subset b a) else false
 ;;
-
-let equal_sets_test_0 = equal_sets [1;2;3] [1;2;3];;
-let equal_sets_test_1 = equal_sets [1;3;2] [1;2;3];;
-let equal_sets_test_2 = equal_sets [] [];;
-let equal_sets_test_3 = not(equal_sets [1] []);;
-let equal_sets_test_3 = not(equal_sets [1] [2]);;
-let equal_sets_test_4 = equal_sets [3;1;3] [1;3];;
 
 (* initially, called as:
     * unique 'a [] 'a list
@@ -53,17 +35,9 @@ let unique lst =
     | [] -> []
 ;;
 
-let unique_test_0 = unique [1;2;3];;
-let unique_test_1 = unique [2;2];;
-let unique_test_2 = unique [];;
-
 let proper_subset a b = 
     if (unique a = unique b) then false else (subset a b)
 ;;
-
-let proper_subset_test0 = proper_subset [] [1;2;3]
-let proper_subset_test1 = proper_subset [3;1;3] [1;2;3]
-let proper_subset_test2 = not (proper_subset [3] [3;3])
 
 (* returns the set difference of a - b *)
 let rec set_diff_rec before this after other = 
@@ -83,73 +57,24 @@ let set_diff a b =
     | hd::tl -> set_diff_rec [] hd tl b
 ;;
 
-let set_diff_test_0 = (set_diff [] []) = [];;
-let set_diff_test_1 = (set_diff [1] []) = [1];;
-let set_diff_test_2 = (set_diff [1] [1]) = [];;
-let set_diff_test_3 = (set_diff [1] [2]) = [1];;
-let set_diff_test_4 = equal_sets (set_diff [1;2;3] [2]) [1;3];;
-let set_diff_test0 = equal_sets (set_diff [1;3] [1;4;3;1]) []
-let set_diff_test1 = equal_sets (set_diff [4;3;1;1;3] [1;3]) [4]
-let set_diff_test2 = equal_sets (set_diff [4;3;1] []) [1;3;4]
-let set_diff_test3 = equal_sets (set_diff [] [4;3;1]) []
-
 let rec computed_fixed_point eq f x = 
     if (eq (f x) x) then x else (computed_fixed_point eq f (f x))
 ;;
-
-let computed_fixed_point_test0 =
-    computed_fixed_point (=) (fun x -> x / 2) 1000000000 = 0;;
-let computed_fixed_point_test1 =
-    computed_fixed_point (=) (fun x -> x *. 2.) 1. = infinity;;
-let computed_fixed_point_test2 =
-    computed_fixed_point (=) sqrt 10. = 1.;;
-let computed_fixed_point_test3 =
-      ((computed_fixed_point (fun x y -> abs_float (x -. y) < 1.)
-                             (fun x -> x /. 2.)
-                             10.)
-      = 1.25);;
 
 let rec eval_p_times f x p = 
     if (0 < p)  then (eval_p_times f (f x) (p - 1))
                 else x
 ;;
 
-let eval_p_times_test0 = eval_p_times (fun x -> x + x) 3 2;;
-
 let rec computed_periodic_point eq f p x =
     if eq x (eval_p_times f x p) then x
         else (computed_periodic_point eq f p (f x))
 ;;
 
-(* Every point is a periodic point for p=0 *)
-let computed_periodic_point_test0 =
-    computed_periodic_point (=) (fun x -> x / 2) 0 (-1) = -1;;
-let computed_periodic_point_test1 =
-    computed_periodic_point (=) (fun x -> x *. x -. 1.) 2 0.5 = -1.;;
-
-(* END WARMUP *)
-
 type ('nonterminal, 'terminal) symbol =
     | N of 'nonterminal
     | T of 'terminal
 ;;
-
-type giant_nonterminals =
-      | Conversation | Sentence | Grunt | Snore | Shout | Quiet
-
-let giant_rules = 
-    [Snore, [T"ZZZ"];
-    Quiet, [];
-    Grunt, [T"khrgh"];
-    Shout, [T"aooogah!"];
-    Sentence, [N Quiet];
-    Sentence, [N Grunt];
-    Sentence, [N Shout];
-    Conversation, [N Snore];
-    Conversation, [N Sentence; T","; N Conversation]];;
-
-let giant_grammar =
-    Conversation, giant_rules;;
 
 (* given a whitelist, remove all rules where LHS not in whitelist *)
 let rec remove_blind_alleys_rec before rule after whitelist =
@@ -168,9 +93,6 @@ let remove_blind_alleys whitelist rules =
     | [] -> []
     | hd::tl -> List.rev(remove_blind_alleys_rec [] hd tl whitelist)
 ;;
-
-let remove_blind_alleys_test0 = 
-    remove_blind_alleys [Conversation] (snd giant_grammar);;
 
 let is_terminal s = 
     match s with
@@ -209,8 +131,6 @@ let filter_blind_alleys g =
     | (start, rules) -> 
             computed_fixed_point (equal_sets) (make_whitelist) (rules, [])
 ;;
-
-let giant_test0 = filter_blind_alleys giant_grammar = giant_grammar;;
 
 filter_blind_alleys giant_grammar 
 
