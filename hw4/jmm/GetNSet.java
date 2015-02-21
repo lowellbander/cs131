6a5aab8930c5b45662c5cbe5d00ed4c2216d14ca
Lowell Bander <lowellbander@gmail.com>
2015-02-21 12:24:40 -0800

    implemented GetNSet

---
+++ hw4/jmm/GetNSet.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+class GetNSet implements State {
+    private AtomicIntegerArray atomic_value;
+    private byte maxval;
+
+    /* given an array of bytes, returns an AtomicIntegerArray*/
+    private AtomicIntegerArray aiaFromBytes(byte[] v) {
+        AtomicIntegerArray a = new AtomicIntegerArray(v.length);
+        for (int i = 0; i < v.length; ++i) a.set(i, (int)v[i]);
+        return a;
+    }
+
+    GetNSet(byte[] v) { 
+        atomic_value = aiaFromBytes(v);
+        maxval = 127; 
+    }
+
+    GetNSet(byte[] v, byte m) { 
+        atomic_value = aiaFromBytes(v);
+        maxval = m; 
+    }
+
+    public int size() { return atomic_value.length(); }
+
+    public byte[] current() { 
+        byte[] b = new byte[size()];
+        for (int i = 0; i < size(); ++i) b[i] = (byte)atomic_value.get(i);
+        return b;  
+    }
+
+    public boolean swap(int i, int j) {
+
+        if (atomic_value.get(i) <= 0 || atomic_value.get(j) >= maxval)
+            return false;
+
+        atomic_value.set(i, atomic_value.get(i) - 1);
+        atomic_value.set(j, atomic_value.get(j) + 1);
+
+	    return true;
+    }
+}
--- hw4/jmm/test.sh
+++ hw4/jmm/test.sh
@@ -3,12 +3,13 @@ nthreads=20
 ntransitions=100
 maxval=10
 
-javac UnsafeMemory.java
+javac UnsafeMemory.java && (
 echo "Running Null test"
 java UnsafeMemory Null $nthreads $ntransitions 10 1 2 3 4 5
 echo "Running Synchronized test"
 java UnsafeMemory Synchronized $nthreads $ntransitions 10 1 2 3 4 5
 echo "Running Unsynchronized test"
 java UnsafeMemory Unsynchronized $nthreads $ntransitions 10 1 2 3 4 5
-#echo "Running GetNSet test"
-#java UnsafeMemory GetNSet $nthreads $ntransitions 10 1 2 3 4 5
+echo "Running GetNSet test"
+java UnsafeMemory GetNSet $nthreads $ntransitions 10 1 2 3 4 5
+) && bash clean.sh
