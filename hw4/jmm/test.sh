5f73a3825ff4b1e9edd91906f37d6f10312c9cce
Lowell Bander <lowellbander@gmail.com>
2015-02-21 10:53:39 -0800

    don't need to compile each time

--- hw4/jmm/test.sh
+++ hw4/jmm/test.sh
@@ -3,9 +3,12 @@ nthreads=20
 ntransitions=100
 maxval=10
 
+javac UnsafeMemory.java
 echo "Running Null test"
-javac UnsafeMemory.java && java UnsafeMemory Null $nthreads $ntransitions 10 1 2 3 4 5
+java UnsafeMemory Null $nthreads $ntransitions 10 1 2 3 4 5
 echo "Running Synchronized test"
-javac UnsafeMemory.java && java UnsafeMemory Synchronized $nthreads $ntransitions 10 1 2 3 4 5
+java UnsafeMemory Synchronized $nthreads $ntransitions 10 1 2 3 4 5
 echo "Running Unsynchronized test"
-javac UnsafeMemory.java && java UnsafeMemory Unsynchronized $nthreads $ntransitions 10 1 2 3 4 5
+java UnsafeMemory Unsynchronized $nthreads $ntransitions 10 1 2 3 4 5
+#echo "Running GetNSet test"
+#java UnsafeMemory GetNSet $nthreads $ntransitions 10 1 2 3 4 5
