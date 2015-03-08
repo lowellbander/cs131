731ad0d36481fb81a5f8de22e324835f1a0bd2db
Lowell Bander <lowellbander@gmail.com>
2015-03-08 06:15:03 -0700

    sets port

--- hw6/server.py
+++ hw6/server.py
@@ -1,3 +1,4 @@
+import sys
 from twisted.internet import protocol, reactor, endpoints
 
 # simplest possible protocol
@@ -10,10 +11,21 @@ class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Echo()
 
+def usage():
+    print "usage: ", sys.argv[0],  " <servername> <port>"
+
 def main():
+
+    if len(sys.argv) != 3: 
+        usage()
+        return
+    
+    servername = sys.argv[1]
+    port = sys.argv[2]
+
     factory = protocol.ServerFactory()
     factory.protocol = Echo
-    reactor.listenTCP(8000, factory)
+    reactor.listenTCP(int(port), factory)
     reactor.run()
 
 # only runs if module wasn't imported
